ec36a235e4f147c726f9f9356b74d98d608cd62f
eridgers <emily.ridgers@gmail.com>
2020-01-10 20:49:23 +0000

    create index and show views for category populate new views from categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,39 @@
 var Category = require('../models/category');
+var Item = require('../models/item');
+var mongoose = require('mongoose');
+var async = require('async');
 
 // category list
 exports.category_list = function(req, res, next){
-    res.send('CATEGORY LIST');
+    Category.find({}).exec(function(err, categories){
+        if(err) {return next(err);}
+        if(categories==null){
+            res.send('NO CATEGORIES');
+        }
+        res.render('../views/categories/index', {title: 'Explore Categories!', categories: categories});
+    })
 };
+
 // one category
 exports.category_detail = function(req, res, next){
-    res.send('CATEGORY DETAIL');
+    var id = mongoose.Types.ObjectId(req.params.id);
+    // need to search on category passed from link?
+    async.parallel({
+        category: function(callback){
+            Category.findById(id).exec(callback);
+        },
+        items: function(callback){
+            Item.find({'category': id}).populate('brand').exec(callback);
+        },
+    }, function(err, results){
+        if(err) {return next(err);}
+        if(results.category==null){
+            var err = new Error('Category not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('../views/categories/show', {title: results.category.name, items: results.items});
+    });
 };
 
 // create GET
