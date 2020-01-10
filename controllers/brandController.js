c2dd149f966e6c9acee3b6ef0b75cc0e8b613eac
eridgers <emily.ridgers@gmail.com>
2020-01-10 20:46:53 +0000

    create index and show views for Brands populate new views in brandController

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,12 +1,36 @@
 var Brand = require('../models/brand');
+var Item = require('../models/item');
+var async = require('async');
 
 // list all brands GET 
 exports.brand_list = function(req, res, next){
-    res.send('BRAND LIST');
+    Brand.find({}).exec(function(err, brands){
+        if(err) {return next(err);}
+        if(brands==null){
+            res.send('NO BRANDS');
+        }
+        res.render('../views/brands/index', {title: 'Browse Brands', brands: brands});
+    })
 };
+
 // get one brand GET
 exports.brand_detail = function(req, res, next){
-    res.send('BRAND DETAIL');
+    async.parallel({
+        brand: function(callback){
+            Brand.findById(req.params.id).exec(callback);
+        },
+        items: function(callback){
+            Item.find({'brand': req.params.id}).populate('category').exec(callback);
+        },
+    }, function(err, results){
+        if(err) {return next(err);}
+        if(results.brand==null){
+            var err = new Error('Brand not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('../views/brands/show', {title: results.brand.name, brand: results.brand, items: results.items});
+    });
 };
 
 // create brand GET
