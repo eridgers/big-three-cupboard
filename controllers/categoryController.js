5237748dea1b92961cff895b0561591703fe2f91
eridgers <emily.ridgers@gmail.com>
2020-01-31 21:37:21 +0000

    add category create page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,6 +3,9 @@ var Item = require('../models/item');
 var mongoose = require('mongoose');
 var async = require('async');
 
+const { check, validationResult } = require('express-validator');
+const { sanitizeBody } = require('express-validator');
+
 // category list
 exports.category_list = function(req, res, next){
     Category.find({}).exec(function(err, categories){
@@ -38,12 +41,41 @@ exports.category_detail = function(req, res, next){
 
 // create GET
 exports.category_new = function(req, res, next){
-    res.send('CATEGORY CREATE GET');
+    res.render('../views/categories/category_form', {title: 'Create Category of Gear'});
 };
+
 // create POST
-exports.category_create = function(req, res, next){
-    res.send('CATEGORY CREATE POST');
-};
+exports.category_create = [
+    //validate fields using express-validator
+    check('name').isLength({min: 1}).withMessage('Name must not be empty').matches(/^[a-z0-9 ]+$/i).withMessage('Category must be reasonable').trim(),
+    check('description').isLength({min: 10}).withMessage('Description must be at least 10 characters').matches(/^[a-z0-9 ]+$/i).trim(),
+    //santize
+    sanitizeBody('*').escape(),
+    //process request
+    (req, res, next) => {
+        //extract validation errors
+        const errors = validationResult(req);
+        //create new Brand object with trimmed/escaped data
+        var category = new Category({
+            name: req.body.name,
+            description: req.body.description,
+            //placeholder image
+            image: ' '
+        })
+        console.log(category);
+        console.log(errors);
+        //check for errors, if yes render form again
+        if(!errors.isEmpty()){
+            res.render('../views/categories/category_form', {title: 'Create Category of Gear', category: category, errors: errors.array()});
+        }else{
+            //else data valid so try to save item to DB
+            category.save(function(err){
+                if(err) {return next(err);}
+                res.redirect(category.url);
+            });
+        }
+    }
+];
 
 // update GET (edit)
 exports.category_edit = function(req, res, next){
