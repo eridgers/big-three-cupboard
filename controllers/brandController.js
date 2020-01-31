4e56eaf1e4215f8982bc331dccff5d390ba7d61e
eridgers <emily.ridgers@gmail.com>
2020-01-31 21:37:03 +0000

    add Brand create page

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -2,6 +2,9 @@ var Brand = require('../models/brand');
 var Item = require('../models/item');
 var async = require('async');
 
+const { check, validationResult } = require('express-validator');
+const { sanitizeBody } = require('express-validator');
+
 // list all brands GET 
 exports.brand_list = function(req, res, next){
     Brand.find({}).exec(function(err, brands){
@@ -35,12 +38,40 @@ exports.brand_detail = function(req, res, next){
 
 // create brand GET
 exports.brand_new = function(req, res, next){
-    res.send('BRAND CREATE GET');
+    res.render('../views/brands/brand_form', {title: 'Create Brand'});
 };
 // create brand POST
-exports.brand_create = function(req, res, next){
-    res.send('BRAND CREATE POST');
-};
+exports.brand_create = [
+    //validate fields using express-validator
+    check('name').isLength({min: 1}).withMessage('Brand name must not be empty').trim(),
+    check('description').isLength({min: 10}).withMessage('Description must be at least 10 characters').trim(),
+    //santize
+    sanitizeBody('*').escape(),
+    //process request
+    (req, res, next) => {
+        //extract validation errors
+        const errors = validationResult(req);
+        //create new Brand object with trimmed/escaped data
+        var brand = new Brand(
+            {
+                name: req.body.name,
+                description: req.body.description,
+                //placeholder logo
+                logo: ' '
+            });
+        //check for errors, if yes render form again
+        if(!errors.isEmpty()){
+            res.render('../views/brands/brand_form' ,{title: 'Create Brand', brand: brand, errors: errors.array()});
+        }else{
+            //else data valid so try to save item to DB
+            brand.save(function(err){
+                if(err) {return next(err);}
+                //check errors if success redirect to new brand
+                res.redirect(brand.url);
+            });
+        }
+    }   
+];
 
 // delete brand POST
 exports.brand_delete = function(req, res, next){
